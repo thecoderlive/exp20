8be4886d29db7c21c409756b77d0c8d6405d1f0a
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 03:16:12 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "2f7401f3-9d5b-4fdc-8974-55ff3759329b",
+    "home_feed": [
+        {
+            "id": "e8bd7f7c-9deb-4c29-8ee0-0f6395f7fc5a",
+            "artist_name": "common",
+            "home_carousel": [
+                {
+                    "id": "c7f57ccf-addd-4fa5-8e6c-c371688a7455",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "dca89a15-cf91-4cd9-abd4-bf3b2f262987",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "7934be37-72f9-49a4-864a-75bcd1080017",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "5a3fc48d-ba5e-41cb-8e89-828e1c8ea79f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "61d6a490-51cf-4979-88eb-83842ebcae1d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                }
+            ],
+            "art_caption": "shake",
+            "art_description": "Significant tonight claim audience. Test only choice step family election history girl."
+        },
+        {
+            "id": "32d6d7b9-ead0-4268-b667-e3f57ffc52c6",
+            "artist_name": "brother",
+            "home_carousel": [
+                {
+                    "id": "4ad5c568-bc3d-4d95-a8c5-ad2196a1a141",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                },
+                {
+                    "id": "158219ab-7bfa-47af-b602-3d1767c385ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "002d9f54-866f-4fbd-9c9a-1dda668f903d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "541ef889-e30f-4fde-988a-382782190edf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "6cd11ce1-16d8-42d5-a80e-390e67969674",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                }
+            ],
+            "art_caption": "PM",
+            "art_description": "Less send popular. West fly write part stage keep participant."
+        },
+        {
+            "id": "962b7b4f-7d56-45f3-b17e-7789a35b41b2",
+            "artist_name": "though",
+            "home_carousel": [
+                {
+                    "id": "4a53d98f-351b-4bc1-9f54-867cc08cd9ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "0c620156-bdc5-4817-b6c2-6180624fd234",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "15203b5a-ca68-4262-a638-7463a8506b6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                },
+                {
+                    "id": "8ca0593f-146c-4e70-8a97-9d9c87f4c3e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "8d590181-47e2-45fc-9905-d728578c090f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                }
+            ],
+            "art_caption": "seat",
+            "art_description": "Man candidate radio guess imagine staff. Each business size watch catch social house center. Detail life weight. Lose upon bill."
+        },
+        {
+            "id": "690550f1-df4b-40a7-818e-f5f8aa46380f",
+            "artist_name": "raise",
+            "home_carousel": [
+                {
+                    "id": "54626adf-5772-4030-915e-f6cb90e302ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "35343ef4-cc73-40da-8519-c4db864a6a03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "5f269aa7-494f-45d4-9e88-852d2d7df64b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "1c277611-ab8b-4f66-93a5-be100bc49ae4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "5d92f818-a8ba-4293-bba3-93a3ab81eeda",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                }
+            ],
+            "art_caption": "guy",
+            "art_description": "Sister expert example trade well win window relate. Impact build help."
+        },
+        {
+            "id": "755624b2-c68d-4f95-9bee-20111182fce5",
+            "artist_name": "use",
+            "home_carousel": [
+                {
+                    "id": "7d51543e-8b88-4fd6-a0ff-18a866224b9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                },
+                {
+                    "id": "d9c697e6-e5af-4ec7-826f-af85074c98de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "adeaef9f-11af-46a8-9de8-e62598422a03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "e2d362fa-8b84-4e96-a892-8fc680b025af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "57fc7443-284e-490d-8578-fe1541a51469",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                }
+            ],
+            "art_caption": "Congress",
+            "art_description": "Eat line care dinner industry event consumer. These sister south pull perform."
+        }
+    ]
+}
